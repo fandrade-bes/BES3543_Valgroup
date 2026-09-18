9047cad3ba4abfd5a8edef9dd2e4a9652518d0b5
fandrade-bes <fandrade@bes.com.br>
2026-09-18 14:11:24 -0300

    STARTUP 18/09
    PLC:
    - Corrigido inconsistências para comissionamento
    - Refeito rotinas de EST_ENTRADA e EST_ENVOLVEDOR
    
    IHM
    - Ajuste de TAGs que ainda estavam com nome antigo

--- IHM/IHM_BES_3543/json/page.combined.js
+++ IHM/IHM_BES_3543/json/page.combined.js
@@ -1,6 +1,38 @@
-$hmi.addPagesScript({// ------------------------------------  LA_Receitas  ------------------------------ 
+$hmi.addPagesScript({// ------------------------------------  LA_Operacao - Empilhador  ------------------------------ 
 
-"m11_page11": function(){
+"m2_page2": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getActivePage().__eval__ = function(a){return eval(a);}
+var project = $hmi.getProjectInterface();
+var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
+page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+,
+// ------------------------------------  LA_Parametros - Empilhador Garra  ------------------------------ 
+
+"m12_page12": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getActivePage().__eval__ = function(a){return eval(a);}
+var project = $hmi.getProjectInterface();
+var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
+page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+,
+// ------------------------------------  LA_Parametros - Empilhador Movimento  ------------------------------ 
+
+"m13_page13": function(){
 // ------------------------------------  Header ------------------------------
 var $=null;var jQuery=null;
 $hmi.getActivePage().__eval__ = function(a){return eval(a);}
@@ -23,6 +55,22 @@ $hmi.getActivePage().__eval__ = function(a){return eval(a);}
 var project = $hmi.getProjectInterface();
 var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
 page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+,
+// ------------------------------------  Salvar Receita  ------------------------------ 
+
+"d1_page21": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getDialog("d1_page21").__eval__ = function(a){return eval(a);}
+var project=$hmi.getProjectInterface();
+var page=new $hmi.defs.hmiIPage($hmi.getDialog("d1_page21"));
+
 var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
 // --------------------------------------User code -------------------------------
 // ------------------------------------  Footer  ------------------------------
